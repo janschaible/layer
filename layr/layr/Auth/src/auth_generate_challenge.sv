1aa6d4e74a169308a131e6c3204a35e152f3ac8e
Thomas Gingele <b1tc0r3@proton.me>
2026-02-11 16:58:53 +0100

    Add key write for aes core

--- layr/layr/Auth/src/auth_generate_challenge.sv
+++ layr/layr/Auth/src/auth_generate_challenge.sv
@@ -32,13 +32,34 @@ module auth_generate_challenge(
         end
     end
 
-    // TODO, maybe: When AES key cannot be permanently set during
-    // initialization, set it now.
+    // TODO: set ecb 128 bit decrypt mode for aes core, key is already set
+    // TODO: set init bit for aes core
+    // TODO: set init bit for aes core
+    /*
+    cs          = 1'b1;
+    we          = 1'b1;
+    address     = 8'h08;
+    write_data  = 32'h1;
+    */
     // TODO: decrypt reg_input_chiper (AES ECB)
     // TODO: obtain rc
     // TODO: get random value rt (NFC reader apparently has RNG)
-    // TODO: Send rc and rt to auth_verify_id.
+    // TODO: set ecb 128 bit encrypt mode for aes core, key is already set
+    /*
+    cs = 1'b1;
+    we = 1'b1;
+    aes_address = 8'h0a;
+    write_data = 32'h0;
+    */
+    // TODO: set init bit for aes core
+    /*
+    cs          = 1'b1;
+    we          = 1'b1;
+    address     = 8'h08;
+    write_data  = 32'h1;
+    * */
     // TODO: create AES_psk(rt || rc)
+    // TODO: send AES_psk(rt || rc) to auth_verify_id
     // TODO: store AES_psk(rt || rc) in reg_challenge_response
 
     // TODO: Placeholder value
--- layr/layr/Auth/src/auth_init.sv
+++ layr/layr/Auth/src/auth_init.sv
@@ -1,8 +1,12 @@
 module aes_init(
     input logic ckl,
     input logic rst,
-    output logic busy,
-    output logic done,
+    output logic busy, // to system controller
+    output logic done, // to system controller
+    output logic cs, // to aes core
+    output logic we, // to aes core
+    output logic [7:0] aes_address, // to aes core
+    output logic [31:0] write_data // to aes core
 );
 
     typedef enum logic {
@@ -11,12 +15,34 @@ module aes_init(
         CONFIG
     } state_t;
 
-     state_t current_state, next_state;
+    state_t current_state, next_state;
+
+    reg [31:0] reg_key [0:7];
+    reg key_index;
 
     always_ff @(posedge clk or posedge rst) begin
         if (rst) begin
             current_state <= FETCH;
+            key_index <= 4'd0;
+            encryption_mode_set <= 1'b0;
+
+        // Handle key index while writing key to aes core.
+        end else if (current_state == CONFIG) begin
+            if (key_index == 4'd7) begin
+                key_index <= 4'd0;
+                current_state <= IDLE;
+                busy = 1'b0;
+                done = 1'b1;
+            end else begin
+                key_index <= key_index + 4'd1;
+            end
+        end else if (current_state == FETCH) begin
+            // TODO: handle write to reg_key, can also use key_index.
+            // TODO: reset key_index once write is finished.
+            // TODO: Is it possible to directly direct the read data from the
+            //       EEPROM to the aes core? would skip storing it locally.
         end else begin
+            key_index <= 4'd0;
             current_state <= next_state;
         end
     end
@@ -40,12 +66,14 @@ module aes_init(
             end
 
             CONFIG: begin
-                // TODO: Set cipher mode for aes core
-                // TODO: set key for aes core
-                // TODO: Do remaining AES stuff
-                next_state = IDLE;
-                busy = 1'b0;
-                done = 1'b1;
+                //  Key addresses (32 bit write bus):
+                //      start: 8'h10
+                //      end:   8'h17
+                busy = 1'b1;
+                cs = 1'b1;
+                we = 1'b1;
+                aes_address = 8'h10 + key_index;
+                write_data = reg_key[key_index];
             end
 
             default: begin
