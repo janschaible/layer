0658569c027d3ca63851ef259051393629f68317
Thomas Gingele <b1tc0r3@proton.me>
2026-02-12 18:23:32 +0100

    [WIP] work started on generate_challenge

--- layr/layr/Auth/src/auth_generate_challenge.sv
+++ layr/layr/Auth/src/auth_generate_challenge.sv
@@ -2,7 +2,7 @@ module auth_generate_challenge(
     input logic clk,
     input logic rst,
     input logic ready_i,
-    input logic [127:0] input_cipher_i,
+    input reg [127:0] input_cipher_i,
 
     output logic error_o,
     output logic challenge_valid_o,
@@ -21,23 +21,78 @@ module auth_generate_challenge(
         SEND
     } current_state, next_state;
 
-    reg [127:0] reg_input_chiper;
-    reg cs;
-    reg we;
+    enum {
+        DECRYPT_SET_MODE,
+        DECRYPT_WRITE,
+        DECRYPT_RUN,
+        DECRYPT_READ
+    } decrypt_current_state, decrypt_next_state;
 
-    aes aes(
-        .cs(cs),
-        .we(we)
-    );
+    enum {
+        ENCRYPT_SET_MODE,
+        ENCRYPT_WRITE_CR,
+        ENCRYPT_READ_CR,
+        ENCRYPT_WRITE_SK,
+        ENCRYPT_READ_SK,
+        ENCRYPT_RUN,
+    } encrypt_current_state, encrypt_next_state;
+
+    reg [2:0] key_index;
 
     always_ff @(posedge clk or posedge rst) begin
+        current_state <= next_state;
+
         if (rst) begin
+            key_index <= 3'd0;
             current_state <= IDLE;
+            decrypt_current_state <= DECRYPT_SET_MODE;
+            encrypt_current_state <= ENCRYPT_SET_MODE;
+            decrypt_next_state <= DECRYPT_WRITE;
+            encrypt_next_state <= ENCRYPT_WRITE_CR;
 
         end else if (current_state == IDLE) begin
             if (ready_i) begin
                 reg_input_chiper <= input_cipher_i;
                 current_state <= DECRYPT;
+            end
+
+        end else if (current_state == DECRYPT) begin
+            if (decrypt_current_state == DECRYPT_SET_MODE) begin
+                // TODO
+            end else if (decrypt_current_state == DECRYPT_WRITE) begin
+                if (data_index == 8'd4) begin
+                    // TODO: Clean up variables
+
+                end else begin
+                    // TODO: Write cipher into aes core
+
+                end
+            end
+        end
+    end
+
+    always_comb begin
+        if (current_state == DECRYPT) begin
+            if (decrypt_current_state == DECRYPT_SET_MODE) begin
+
+            end else if (decrypt_current_state == DECRYPT_WRITE) begin
+                if (data_index == 8'd4) begin
+                    // TODO: Clean up variables
+
+                end else begin
+                    // TODO: Write cipher into aes core
+
+                end
+
+            end else if (decrypt_current_state == DECRYPT_RUN) begin
+                aes_address_o = 8'h08;
+                aes_write_data_o = 32'h1;
+                aes_cs_o = 1'b1;
+                aes_we_o = 1'b1;
+
+                // TODO: Wait for run to finish
+
+            end else if (decrypt_current_state == DECRYPT_READ) begin
             end
         end
     end
